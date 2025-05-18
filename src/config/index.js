4592faf2c5ae4d43ee78c40db8161126e80c5343
eao <1119346628@qq.com>
2025-05-18 09:29:25 +0000

    aicore init

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,13 @@
 export const config = {
-  apiBaseUrl: 'http://127.0.0.1:5000',
-  socketUrl: 'http://127.0.0.1:5000',
+  apiBaseUrl: "https://xidfvznrxshn.sealoshzh.site",
+  socketUrl: "https://xidfvznrxshn.sealoshzh.site",
   socketOptions: {
-    transports: ['websocket', 'polling'],
+    transports: ["websocket", "polling"],
     autoConnect: true,
     reconnection: true,
     reconnectionDelay: 1000,
     reconnectionDelayMax: 5000,
     reconnectionAttempts: 5,
-    timeout: 20000
-  }
-}; 
+    timeout: 20000,
+  },
+};
